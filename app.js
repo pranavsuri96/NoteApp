fa14b331123b044369b3c5d0b150c826ca4dca28
pranavsuri96 <pranavsuri96@gmail.com>
2024-12-12 17:34:50 +0530

    CHANGES

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 if (typeof document !== 'undefined') {
   // Azure Blob Storage configuration
+  const { BlobServiceClient } = AzureStorageBlob;
   const storageAccountUrl = "https://memonote.blob.core.windows.net";
   const containerName = "notes";
   const sasToken = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-12-12T13:55:38Z&st=2024-12-12T05:55:38Z&spr=https,http&sig=FrTfeNewJuzN1WtVcQ3qKsFA4H%2BgOdplOkVlsqTtEVk%3D";
@@ -7,8 +8,9 @@ if (typeof document !== 'undefined') {
 // <script src="https://cdn.jsdelivr.net/npm/@azure/storage-blob@12.15.0/dist/azure-storage-blob.min.js"></script>
 
 // Initialize BlobServiceClient
-const blobServiceClient = new Azure.StorageBlob.BlobServiceClient(`${storageAccountUrl}?${sasToken}`);
+//const blobServiceClient = new Azure.StorageBlob.BlobServiceClient(`${storageAccountUrl}?${sasToken}`);
 
+const blobServiceClient = new BlobServiceClient(`${storageAccountUrl}?${sasToken}`);
 // Example: List blobs in the container
 async function listBlobs() {
   const containerName = "notes";
